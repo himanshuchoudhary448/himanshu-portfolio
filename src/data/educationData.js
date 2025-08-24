66428e0b6036dd0ee5be730c6d0822fd9e5ce100
Himanshu Choudhary <himanshuchoudhary448@gmail.com>
2025-08-24 16:46:17 -0400

    Update education details

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -2,14 +2,14 @@ export const educationData = [
     {
         id: 1,
         institution: 'New York Institute of Technology',
-        course: 'Master of Science, Computer Science',
+        course: 'M.S. Computer Science',
         startYear: 'September 2018',
         endYear: 'May 2020'
     },
     {
         id: 2,
         institution: 'Rajiv Gandhi Proudyogiki Vishwavidyalaya',
-        course: 'Bachelor of Engineering, Computer Science',
+        course: 'B.E. Computer Science',
         startYear: 'August 2014',
         endYear: 'June 2018'
     }
